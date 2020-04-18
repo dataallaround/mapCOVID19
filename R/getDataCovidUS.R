cf51c0529e5bceebb4738505a3275ec4364a5a80
Alessio Serafini <srf.alessio@gmail.com>
2020-04-18 13:11:31 +0200

    US

---
+++ R/getDataCovidUS.R
@@ -0,0 +1,42 @@
+
+
+getDataCovidUS <- function(date, level = c("states"), download_dir = NULL)
+{
+  
+  library(stringr)
+  
+  level <- match.arg(level, choices = eval(formals(getDataCovidIT)$level))
+  date <- str_replace_all(string = date, pattern = "-", replacement = "")
+  ndt <- length(date)
+  
+  url <- paste("https://raw.githubusercontent.com/dataallaround/mapCOVID19/master/US/RData/", level, sep = "")
+  
+  temp <- vector("list", ndt)
+  
+  if(!is.null(download_dir))
+  {
+    for(i in 1:ndt)
+    {
+      file <- paste("ita_",level,"_",date[i], sep = "")
+      file_url <- paste(url,file,sep = "/")
+      download.file(url, download_dir)
+      temp[[i]] <- get(load(paste(download_dir,file,sep = "/")))
+    }
+    
+  }else{
+    
+    for(i in 1:ndt)
+    {
+      file <- paste("ita_",level,"_", date[i], ".RData",sep = "")
+      file_url <- paste(url,file,sep = "/")
+      temp[[i]] <- get(load(url(file_url)))
+    }
+    
+    
+  }
+  
+  out <- do.call(rbind,temp)
+  
+  return(out)
+  
+}
