041c9342060c4326b11fda58aa56c05a66b7593e
Alessio Serafini <srf.alessio@gmail.com>
2020-04-24 03:34:32 +0200

    Add FromTo argument and add IT 2020-04-22

--- R/getDataCovid_US.R
+++ R/getDataCovid_US.R
@@ -1,41 +1,91 @@
 
 
-getDataCovid_US <- function(date, level = c("states"), download_dir = NULL)
+getDataCovid_US <- function(date = NULL, FromTo = NULL, by = c("day", "week", "month"),
+                            level = c("states"), download_dir = NULL)
 {
 
-  library(stringr)
+  require(stringr)
+  require(httr)
 
   level <- match.arg(level, choices = eval(formals(getDataCovid_US)$level))
-  date <- str_replace_all(string = date, pattern = "-", replacement = "")
-  ndt <- length(date)
 
-  url <- paste("https://raw.githubusercontent.com/dataallaround/mapCOVID19/master/US/RData", sep = "")
+  if(is.null(date) & is.null(FromTo)){
 
-  temp <- vector("list", ndt)
+    url <- paste("https://raw.githubusercontent.com/dataallaround/mapCOVID19/master/", sep = "")
+    list <- GET("https://api.github.com/repos/dataallaround/mapCOVID19/git/trees/master?recursive=1")
+    stop_for_status(list)
+    file <- unlist(lapply(content(list)$tree, "[", "path"), use.names = F)
+    file <- grep(paste("US/RData/",level, "/us_",level, sep = ""), file, value = TRUE, fixed = TRUE)
 
-  if(!is.null(download_dir))
-  {
-    for(i in 1:ndt)
+
+    ndt <- length(file)
+    temp <- vector("list", ndt)
+
+    if(!is.null(download_dir))
     {
-      file <- paste("us_",level,"_",date[i], sep = "")
-      file_url <- paste(url,file,sep = "/")
-      download.file(url, download_dir)
-      temp[[i]] <- get(load(paste(download_dir,file,sep = "/")))
-    }
+      for(i in 1:ndt)
+      {
+        file_url <- paste(url,file[i],sep = "")
+        download.file(url, download_dir)
+        temp[[i]] <- get(load(paste(download_dir,file[i],sep = "/")))[,]
+      }
 
+    }else{
+
+      for(i in 1:ndt)
+      {
+        file_url <- paste(url,file[i],sep = "")
+        temp[[i]] <- get(load(url(file_url)))
+      }
+
+
+    }
   }else{
 
-    for(i in 1:ndt)
+    by <- match.arg(by, choices = eval(formals(getDataCovid_US)$by))
+    url <- paste("https://raw.githubusercontent.com/dataallaround/mapCOVID19/master/US/RData", sep = "")
+
+    if(!is.null(FromTo)){
+
+      from <- as.Date(FromTo[1])
+      to <- as.Date(FromTo[2])
+
+      date <- seq.Date(from = from, to = to, by = by)
+
+    }
+
+    date <- str_replace_all(string = date, pattern = "-", replacement = "")
+    ndt <- length(date)
+
+
+    temp <- vector("list", ndt)
+
+    if(!is.null(download_dir))
     {
-      file <- paste("us_",level,"_", date[i], ".RData",sep = "")
-      file_url <- paste(url,file,sep = "/")
-      temp[[i]] <- get(load(url(file_url)))
+      for(i in 1:ndt)
+      {
+        file <- paste("us_",level,"_",date[i], sep = "")
+        file_url <- paste(url,file,sep = "/")
+        download.file(url, download_dir)
+        temp[[i]] <- get(load(paste(download_dir,file,sep = "/")))
+      }
+
+    }else{
+
+      for(i in 1:ndt)
+      {
+        file <- paste("us_",level,"_", date[i], ".RData",sep = "")
+        file_url <- paste(url,file,sep = "/")
+        temp[[i]] <- get(load(url(file_url)))
+      }
+
+
     }
 
 
   }
 
-  out <- do.call(rbind,temp)
+  out <- do.call(rbind.data.frame,temp)
 
   return(out)
 
